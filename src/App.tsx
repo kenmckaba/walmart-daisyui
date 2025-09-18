6b8aadc8c02f705bbb3473a6b8ccd9debb68e610
kenmckaba <ken@mckaba.com>
2025-09-18 16:23:11 -0700

    initial

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+	return (
+		<>
+			<div className="btn btn-primary">Hello Vite + React!</div>
+			<div className="btn btn-secondary">Hello Vite + React!</div>
+		</>
+	)
+}
+
+export default App
