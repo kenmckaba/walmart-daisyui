4b2b09c4d3bb3949932b8dac8631a55dc0e0ecf9
kenmckaba <ken@mckaba.com>
2025-09-18 21:37:20 -0700

    header with select category

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,11 @@
 import { CategoryList } from './CategoryList'
+import walmartstar from '../assets/walmart-logo.png'
 
 export const Header = () => {
 	return (
-		<>
-			<div>Header</div>
+		<div className="flex h-15 flex-row gap-1 items-center justify-center border-1 rounded-md">
+			<img className="h-12" src={walmartstar} alt="Walmart Logo" />
 			<CategoryList />
-		</>
+		</div>
 	)
 }
