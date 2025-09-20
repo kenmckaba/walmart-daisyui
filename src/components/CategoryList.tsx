92362f06642583e42d074df80641d36dbad6bdd9
kenmckaba <ken@mckaba.com>
2025-09-19 20:44:11 -0700

    tweeks

--- src/components/CategoryList.tsx
+++ src/components/CategoryList.tsx
@@ -8,6 +8,9 @@ export const CategoryList = () => {
 
 	return (
 		<select className="bg-gray-50 border w-40 border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+			<option disabled selected hidden>
+				Select Category
+			</option>
 			{categories?.map((cat: Category) => (
 				<option key={cat.slug} value={cat.slug}>
 					{cat.name}
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,10 @@
-import { CategoryList } from './CategoryList'
 import walmartstar from '../assets/walmart-logo.png'
+import { CategoryList } from './CategoryList'
 
 export const Header = () => {
 	return (
 		<div className="flex h-15 flex-row gap-1 items-center justify-center border-1 rounded-md">
-			<img className="h-12" src={walmartstar} alt="Walmart Logo" />
+			<img className="h-18" src={walmartstar} alt="Walmart Logo" />
 			<CategoryList />
 		</div>
 	)
