dfdffd29973f8e4695a601883d44051b86257cc7
kenmckaba <ken@mckaba.com>
2025-09-24 21:23:47 -0700

    added cart modal

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -1,24 +1,43 @@
-import type { Product } from './hooks/useProducts'
 // import { ImageList } from './ImageList'
+import { useId } from 'react'
+import type { Product } from './hooks/useProducts'
+import { ProductModal } from './ProductModal'
 
 type ProductCardProps = {
 	product: Product
 }
 
-export const ProductCard = ({ product }: ProductCardProps) => (
-	<div className="card bg-base-100 w-64 shadow-sm">
-		<figure>
-			<img key={product.images[0]} src={product.images[0]} alt={'blah'} />
-			{/* <ImageList images={product.images} title={product.title} /> */}
-		</figure>
-		<div className="card-body">
-			<h2 className="card-title">{product.title}</h2>
-			<p>{product.description}</p>
-			<div className="card-actions justify-end">
-				<button type="button" className="btn btn-primary">
-					Buy Now
-				</button>
+export const ProductCard = ({ product }: ProductCardProps) => {
+	const modalId = useId()
+	const modal = document.getElementById(modalId) as HTMLDialogElement
+
+	const showModal = () => {
+		modal.showModal()
+	}
+
+	return (
+		<>
+			<div className="card bg-base-100 w-64 shadow-sm">
+				<figure>
+					<img key={product.images[0]} src={product.images[0]} alt={'blah'} />
+					{/* <ImageList images={product.images} title={product.title} /> */}
+				</figure>
+				<div className="card-body">
+					<h2 className="card-title">{product.title}</h2>
+					<p>{product.description}</p>
+					<div className="card-actions justify-end">
+						<button
+							type="button"
+							className="btn btn-primary"
+							onClick={showModal}
+						>
+							Add to cart
+						</button>
+					</div>
+				</div>
 			</div>
-		</div>
-	</div>
-)
+
+			<ProductModal modalId={modalId} />
+		</>
+	)
+}
---
+++ src/components/ProductModal.tsx
@@ -0,0 +1,23 @@
+// import { useId } from 'react'
+type ProductModalProps = {
+	modalId: string
+}
+
+export const ProductModal = ({ modalId }: ProductModalProps) => {
+	return (
+		<dialog id={modalId} className="modal">
+			<div className="modal-box">
+				<h3 className="font-bold text-lg">Added to cart!</h3>
+				<p className="py-4">Press ESC key or click the button below to close</p>
+				<div className="modal-action">
+					<form method="dialog">
+						{/* if there is a button in form, it will close the modal */}
+						<button type="submit" className="btn">
+							OK
+						</button>
+					</form>
+				</div>
+			</div>
+		</dialog>
+	)
+}
