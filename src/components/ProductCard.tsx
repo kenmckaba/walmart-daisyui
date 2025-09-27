ade3c4a348d593b3c8e6f51dc0b25f2fd422e7f4
kenmckaba <ken@mckaba.com>
2025-09-27 15:16:11 -0700

    price and total in product

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -1,5 +1,5 @@
 import { ImageList } from './ImageList'
-import { useId } from 'react'
+import { useId, useState } from 'react'
 import type { Product } from './hooks/useProducts'
 // import { ProductModal } from './ProductModal'
 import ShoppingCartModal from './ShoppingCartModal'
@@ -10,11 +10,20 @@ type ProductCardProps = {
 
 export const ProductCard = ({ product }: ProductCardProps) => {
 	const modalId = useId()
+	const [totalCost, setTotalCost] = useState(product.price)
 
 	const showCartModal = () => {
 		const modal = document.getElementById(modalId) as HTMLDialogElement
 		modal.showModal()
 	}
+
+	const handleQuantityChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+		const quantity = parseInt(e.target.value, 10)
+		setTotalCost(quantity * product.price)
+	}
+
+	const priceString = `$${product.price}`
+	const totalString = `$${totalCost}`
 
 	return (
 		<>
@@ -25,13 +34,24 @@ export const ProductCard = ({ product }: ProductCardProps) => {
 				<div className="card-body">
 					<h2 className="card-title">{product.title}</h2>
 					<p>{product.description}</p>
-					<p>Price: {product.price}</p>
 					<div>
-						<div className="flex flex-col items-end p-4 w-20">
-							<p>Quantity:</p>
-							<p>Total:</p>
+						<div className="grid grid-cols-2 grid-rows-3 mr-10 mt-3">
+							<div className="justify-self-end mr-1 self-center">Price:</div>
+							<div className="self-center">{priceString}</div>
+							<div className="justify-self-end mr-1 self-center">Quantity:</div>
+							<div>
+								<input
+									type="number"
+									min={1}
+									className="input"
+									defaultValue="1"
+									self-center
+									onChange={handleQuantityChange}
+								/>
+							</div>
+							<div className="justify-self-end mr-1 self-center">Total:</div>
+							<div className="self-center">{totalString}</div>
 						</div>
-						<input type="number" className="input max-w-xs" />
 					</div>
 					<div className="card-actions justify-end">
 						<button
